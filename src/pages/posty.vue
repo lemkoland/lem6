519839b3f688c8465180ef6cf0138f2beee5ef89
lemkoland <98834856+lemkoland@users.noreply.github.com>
2022-03-24 10:27:08 +0100

    Update posty.vue

--- src/pages/posty.vue
+++ src/pages/posty.vue
@@ -8,6 +8,8 @@
         </g-link>
       </li>
     </ul>
+
+
     <!-- <h1 class="tyt">A oto nasze propozycje na dziś:</h1>
     <ul class="post-list">
       <li v-for="{ node } in $page.allWordPressPost.edges" :key="node.id">
@@ -21,7 +23,7 @@
 <page-query>
 
   query Home ($page: Int) {
-    allWordPressPost (page: $page, perPage: 50) @paginate {
+    allWordPressPost (page: $page, perPage: 100) @paginate {
       pageInfo {
         totalPages
         currentPage
@@ -66,6 +68,15 @@
         }
       }
     }
+    allWordPressCategory (sortBy: "name", order: ASC) {
+  edges {
+    node {
+      title
+      id
+      path
+    }
+  }
+}
   }
 
 
@@ -127,162 +138,170 @@ export default {
     clip-path: polygon(0 0, 100% 0, 100% 0, 0 0);
   }
 
-  .tag-list {
-    max-width: 80vw;
-    display: flex;
-    flex-wrap: wrap;
-    min-height: 10vw;
-    margin-bottom: 20vw;
-   }
-   .tag-list li {
-     padding: 1vw 3vh;
-     gap: 0;
-     margin:0;
-   }
-   .tag-list li a {
-     text-decoration: none;
-   }
-   .tag-list li:nth-child(1) a {
-     color: #784C39;
-   }
-   .tag-list li:nth-child(1):hover {
-        background-color: #784C39;
-        transition: .3s;
-        color: 202020;
-   }
-   .tag-list li:nth-child(2) a {
-     color: #B07156;
-   }
-   .tag-list li:nth-child(2):hover {
-        background-color: #B07156;
-        transition: .3s;
-   }
-   .tag-list li:nth-child(3) a {
-     color: #AE605F;
-   }
-   .tag-list li:nth-child(3):hover {
-        background-color: #AE605F;
-        transition: .3s;
-   }
-   .tag-list li:nth-child(4) a {
-     color: #AB4E68;
-   }
-   .tag-list li:nth-child(4):hover {
-        background-color: #AB4E68;
-        transition: .3s;
-   }
-   .tag-list li:nth-child(5) a {
-     color: #7F4357;
-   }
-   .tag-list li:nth-child(5):hover {
-        background-color: #7F4357;
-        transition: .3s;
-   }
-   .tag-list li:nth-child(6) a {
-     color: #533745;
-   }
-   .tag-list li:nth-child(6):hover {
-        background-color: #533745;
-        transition: .3s;
-   }
-   .tag-list li:nth-child(7) a {
-     color: #A8948A;
-   }
-   .tag-list li:nth-child(7):hover {
-        background-color: #A8948A;
-        transition: .3s;
-   }
-   .tag-list li:nth-child(8) a {
-     color: #3B302B;
-   }
-   .tag-list li:nth-child(8):hover {
-        background-color: #3B302B;
-        transition: .3s;
-   }
-   .tag-list li:nth-child(9) a {
-     color: #9D9171;
-   }
-   .tag-list li:nth-child(9):hover {
-        background-color: #9D9171;
-        transition: .3s;
-   }
-   .tag-list li:nth-child(10) a {
-     color: #554B30;
-   }
-   .tag-list li:nth-child(10):hover {
-        background-color: #554B30;
-        transition: .3s;
-   }
-   .tag-list li:nth-child(11) a {
-     color: #9E8234;
-   }
-   .tag-list li:nth-child(11):hover {
-        background-color: #9E8234;
-        transition: .3s;
-   }
-   .tag-list li:nth-child(12) a {
-     color: #998C69;
-   }
-   .tag-list li:nth-child(12):hover {
-        background-color: #998C69;
-        transition: .3s;
-   }
-   .tag-list li:nth-child(13) a {
-     color: #4E4838;
-   }
-   .tag-list li:nth-child(13):hover {
-        background-color: #4E4838;
-        transition: .3s;
-   }
-   .tag-list li:nth-child(14) a {
-     color: #845440;
-   }
-   .tag-list li:nth-child(14):hover {
-        background-color: #845440;
-        transition: .3s;
-   }
-   .tag-list li:nth-child(15) a {
-     color: #845440;
-   }
-   .tag-list li:nth-child(15):hover {
-        background-color: #845440;
-        transition: .3s;
-   }
-   .tag-list li:nth-child(16) a {
-     color: #DB4F4F;
-   }
-   .tag-list li:nth-child(16):hover {
-        background-color: #DB4F4F;
-        transition: .3s;
-   }
-   .tag-list li:nth-child(17) a {
-     color: #4B1A1A;
-   }
-   .tag-list li:nth-child(17):hover {
-        background-color: #4B1A1A;
-        transition: .3s;
-   }
-   .tag-list li:nth-child(18) a {
-     color: #473527;
-   }
-   .tag-list li:nth-child(18):hover {
-        background-color: #473527;
-        transition: .3s;
-   }
-   .tag-list li:nth-child(19) a {
-     color: #552600;
-   }
-   .tag-list li:nth-child(19):hover {
-        background-color: #552600;
-        transition: .3s;
-   }
-   .tag-list li:nth-child(20) a {
-     color: #A87E5D;
-   }
-   .tag-list li:nth-child(20):hover {
-        background-color: #A87E5D;
-        transition: .3s;
-   }
+
+    .tag-list {
+      max-width: 80vw;
+      display: flex;
+      flex-wrap: wrap;
+      min-height: 10vw;
+      margin-bottom: 20vw;
+      padding: 10rem 10rem;
+
+      /* From https://css.glass  */
+      background: rgba(255, 255, 255, .03);
+      box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+      backdrop-filter: blur(4.5px);
+      -webkit-backdrop-filter: blur(4.5px);
+      border: 1px solid rgba(255, 255, 255, 0.32);
+     }
+     .tag-list li {
+       padding: 1vw 3vh;
+       gap: 0;
+       margin:0;
+     }
+     .tag-list li a {
+       text-decoration: none;
+       padding: 0;
+       margin: 0;
+       width: 100%;
+       height: 100%;
+       color: #fff!important;
+
+     }
+
+     .tag-list li:nth-child(1) {
+
+       background-color: #784C39;
+     }
+     .tag-list li:nth-child(1):hover {
+          background-color: #784C39;
+          transition: .3s;
+          color: 202020;
+
+     }
+     .tag-list li:nth-child(2)  {
+
+                     background-color: #B07156;
+     }
+
+     .tag-list li:nth-child(2):hover {
+          background-color: #B07156;
+          transition: .3s;
+
+     }
+     .tag-list li:nth-child(3) {
+    background-color: #AE605F;
+     }
+     .tag-list li:nth-child(3):hover {
+          background-color: #AE605F;
+          transition: .3s;
+     }
+     .tag-list li:nth-child(4)  {
+
+       background-color: #AB4E68;
+     }
+     .tag-list li:nth-child(4):hover {
+          transition: .3s;
+     }
+     .tag-list li:nth-child(5)  {
+
+       background-color: #7F4357;
+     }
+     .tag-list li:nth-child(5):hover {
+          transition: .3s;
+     }
+     .tag-list li:nth-child(6) {
+
+       background-color: #533745;
+     }
+     .tag-list li:nth-child(6):hover {
+          transition: .3s;
+     }
+     .tag-list li:nth-child(7)  {
+
+       background-color: #A8948A;
+     }
+     .tag-list li:nth-child(7):hover {
+          transition: .3s;
+     }
+     .tag-list li:nth-child(8) {
+       background-color: #3B302B;
+     }
+     .tag-list li:nth-child(8):hover {
+          transition: .3s;
+     }
+     .tag-list li:nth-child(9) {
+       background-color: #9D9171;
+     }
+     .tag-list li:nth-child(9):hover {
+          transition: .3s;
+     }
+     .tag-list li:nth-child(10) {
+       background-color: #554B30;
+     }
+     .tag-list li:nth-child(10):hover {
+          transition: .3s;
+     }
+     .tag-list li:nth-child(11)  {
+       background-color: #9E8234;
+     }
+     .tag-list li:nth-child(11):hover {
+          transition: .3s;
+     }
+     .tag-list li:nth-child(12)  {
+       background-color: #998C69;
+     }
+     .tag-list li:nth-child(12):hover {
+          transition: .3s;
+     }
+     .tag-list li:nth-child(13) {
+       background-color: #4E4838;
+     }
+     .tag-list li:nth-child(13):hover {
+          transition: .3s;
+     }
+     .tag-list li:nth-child(14) {
+       background-color: #845440;
+     }
+     .tag-list li:nth-child(14):hover {
+          transition: .3s;
+     }
+     .tag-list li:nth-child(15) {
+       background-color: #845480;
+     }
+     .tag-list li:nth-child(15):hover {
+          transition: .3s;
+     }
+     .tag-list li:nth-child(16)  {
+       background-color: #DB4F4F;
+     }
+     .tag-list li:nth-child(16):hover {
+          transition: .3s;
+     }
+     .tag-list li:nth-child(17)  {
+       background-color: #4B1A1A;
+     }
+     .tag-list li:nth-child(17):hover {
+          transition: .3s;
+     }
+     .tag-list li:nth-child(18)  {
+       background-color: #473527;
+     }
+     .tag-list li:nth-child(18):hover {
+          transition: .3s;
+     }
+     .tag-list li:nth-child(19)  {
+       background-color: #552600;
+     }
+     .tag-list li:nth-child(19):hover {
+          transition: .3s;
+     }
+     .tag-list li:nth-child(20)  {
+       background-color: #A87E5D;
+     }
+     .tag-list li:nth-child(20):hover {
+          transition: .3s;
+     }
    h1 {
      margin: 10vh auto;
    }
